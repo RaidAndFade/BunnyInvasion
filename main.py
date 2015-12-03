8dba93ec49e476b1ae8d16c865ad01ff74bac1bd
memanlolz <memanlolz@users.noreply.github.com>
2015-12-03 14:00:12 -0500

    FAM

--- main.py
+++ main.py
@@ -9,16 +9,27 @@ screen = pygame.display.set_mode((800,600))
 clock = pygame.time.Clock()
 pygame.font.init()
 
-def drawBunny(x,y):
-    pygame.draw.ellipse(screen, (255, 255, 255), (x, 20 + y, 50, 30))
-    pygame.draw.line(screen, (255, 255, 255), [6 + x, 35 + y], [6 + x, 60 + y], 3)
-    pygame.draw.line(screen, (255, 255, 255), [15 + x, 40 + y], [15 + x, 60 + y], 3)
-    pygame.draw.line(screen, (255, 255, 255), [30 + x, 40 + y], [30 + x, 60 + y], 3)
-    pygame.draw.line(screen, (255, 255, 255), [39 + x, 35 + y], [39 + x, 60 + y], 3)
-    pygame.draw.ellipse(screen, (255, 255, 255), (42 + x, 25 + y, 30, 20))
-    pygame.draw.circle(screen, (0, 0, 0), (64 + x, 32 + y), 2)
-    pygame.draw.ellipse(screen, (255, 255, 255), (50 + x, 10 + y, 12, 20))
-    pygame.draw.line(screen, (0, 0, 0), [60 + x, 40 + y], [69 + x, 40 + y], 1)
+def drawBunny(x,y,dir):
+    if dir == 0:
+        pygame.draw.ellipse(screen, (255, 255, 255), (x, 20 + y, 50, 30)) #body
+        pygame.draw.line(screen, (255, 255, 255), [6 + x, 35 + y], [6 + x, 60 + y], 3) #leg 1
+        pygame.draw.line(screen, (255, 255, 255), [15 + x, 40 + y], [15 + x, 60 + y], 3) #leg 2
+        pygame.draw.line(screen, (255, 255, 255), [30 + x, 40 + y], [30 + x, 60 + y], 3) #leg 3
+        pygame.draw.line(screen, (255, 255, 255), [39 + x, 35 + y], [39 + x, 60 + y], 3) #leg 4
+        pygame.draw.ellipse(screen, (255, 255, 255), (42 + x, 25 + y, 30, 20)) #head
+        pygame.draw.circle(screen, (0, 0, 0), (64 + x, 32 + y), 2) #eye will remembr jews
+        pygame.draw.ellipse(screen, (255, 255, 255), (50 + x, 10 + y, 12, 20)) #faec
+        pygame.draw.line(screen, (0, 0, 0), [60 + x, 40 + y], [69 + x, 40 + y], 1) #mout
+    elif dir == 1:
+        pygame.draw.ellipse(screen, (255, 255, 255), (x, 20 + y, 50, 30))
+        pygame.draw.line(screen, (255, 255, 255), [6 + x, 35 + y], [6 + x, 60 + y], 3)
+        pygame.draw.line(screen, (255, 255, 255), [15 + x, 40 + y], [15 + x, 60 + y], 3)
+        pygame.draw.line(screen, (255, 255, 255), [30 + x, 40 + y], [30 + x, 60 + y], 3)
+        pygame.draw.line(screen, (255, 255, 255), [39 + x, 35 + y], [39 + x, 60 + y], 3)
+        pygame.draw.ellipse(screen, (255, 255, 255), (42 + x, 25 + y, 30, 20))
+        pygame.draw.circle(screen, (0, 0, 0), (64 + x, 32 + y), 2)
+        pygame.draw.ellipse(screen, (255, 255, 255), (50 + x, 10 + y, 12, 20))
+        pygame.draw.line(screen, (0, 0, 0), [60 + x, 40 + y], [69 + x, 40 + y], 1)
     #pygame.draw.rect(screen,(0,0,0),(x,y,69,60))
     return Rect(x,y,69,60)
 
@@ -40,7 +51,7 @@ def checkCollision(player,level):
             colliding = collide.colliderect(player)
             # pygame.draw.rect(screen,(0,0,0),collide);
             if colliding and not player.y == collide.y-player.h:
-                bunnyPos[1] = collide.y-player.h
+                bunnyPos[1] = collide.y-player.h+1
                 return colliding
 
     return colliding
@@ -50,6 +61,7 @@ bunnyPos = [0,0]
 keysDown = [False]*400
 yAccel = 0
 isOnGround = True
+facing = 0
 
 while True :
     clock.tick(60)
@@ -68,9 +80,11 @@ while True :
 
     #game state
     if keysDown[276]: #Left
-        bunnyPos[0] -= 2
+        bunnyPos[0] -= 7
+        facing = 0
     if keysDown[275]: #right
-        bunnyPos[0] += 2
+        bunnyPos[0] += 7
+        facing = 1
     if keysDown[32]: #jump
         if isOnGround:
             yAccel = 20
@@ -83,7 +97,7 @@ while True :
         yAccel -= 1
 
     drawLevel(0,curLevel)
-    bunny = drawBunny(bunnyPos[0],bunnyPos[1])
+    bunny = drawBunny(bunnyPos[0],bunnyPos[1],facing)
 
     isOnGround = checkCollision(bunny,curLevel)
 
